848ff54628b70cabe5163858e49a5dca9e3f5131
D_00 <leo.daloz@epita.fr>
2024-03-19 11:48:03 +0100

    Re-added files from older repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # isocraft-story
+
 Repo for Isocraft Story website
+
+[Visit the website](https://d-002.github.io/isocraft-story)
+
+[Repo for code and other assets](https://github.com/RaphoufouLeFou/Isocraft_Story)
---
+++ script.js
@@ -0,0 +1,82 @@
+let W, H;
+
+let images;
+
+let fps = 60;
+let interval;
+let currentId = 0;
+let start;
+let delay0 = 500, delay1 = 3000;
+let left, right;
+
+// from SO/q/13382516
+function getScrollbarWidth() {
+	// Creating invisible container
+	const outer = document.createElement('div');
+	outer.style.visibility = 'hidden';
+	outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+	outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+	document.body.appendChild(outer);
+
+	// Creating inner element and placing it in the container
+	const inner = document.createElement('div');
+	outer.appendChild(inner);
+
+	// Calculating difference between container's full width and the child width
+	const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+	// Removing temporary elements from the DOM
+	outer.parentNode.removeChild(outer);
+
+	return scrollbarWidth;
+}
+
+smoothstep = t => {return (3-2*t)*t*t};
+
+function fadein() {
+	let t = (Date.now()-start) / delay0;
+	if (t >= 1) {
+		// stop animation, start slides
+		window.clearInterval(interval);
+		start = Date.now()-1000;
+		interval = window.setInterval(update, 1000/fps);
+	} else {
+		left.children[0].style = "transform: opacity(" + (1-smoothstep(t)) + ")";
+	}
+}
+
+function update() {
+	if (document.body.scrollTop > H) return;
+
+	let t = Date.now()-start;
+	if (t >= delay1) {
+		// switch to next image
+		start = Date.now();
+		right.innerHTML = '<img src="'+images[currentId++%images.length].src+'">';
+	}
+	else if (t < 1000) {
+		t = -smoothstep(t/1000)*W;
+		left.children[0].style = "transform: translateX(" + t + "px)";
+		right.children[0].style = "transform: translateX(" + t + "px)";
+	}
+	else {
+		left.innerHTML = right.innerHTML;
+		right.innerHTML = "";
+		left.children[0].style = "";
+	}
+}
+
+function init() {
+	W = window.innerWidth - getScrollbarWidth();
+	H = window.innerHeight;
+	document.body.style = "--W: "+W+"; --H: "+H;
+
+	left = document.getElementById("left");
+	right = document.getElementById("right");
+	images = document.getElementById("bank").children;
+
+	// init slides and start fadein animation
+	left.innerHTML = '<img src="'+images[0].src+'">';
+	start = Date.now();
+	interval = window.setInterval(fadein, 1000/fps);
+}

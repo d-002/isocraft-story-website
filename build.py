17dda39d6f7fb5437309c0db153a0979ccb709af
D_00 <leo.daloz@epita.fr>
2024-06-16 19:01:50 +0200

    Build script bug fixes, edited version

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
-from os import listdir, isdir
-from os.path import join, splitext
+from os import listdir
+from os.path import join, isdir, splitext
 
 # get previous version
 with open('index.html') as f:
     v = f.read().split('?v=')[1].split('"')[0]
 
-v = input('Enter new version (default %s): ' %v)
+v2 = input('Enter new version (default %s): ' %v)
+if v2: v = v2
 
 # edit files
 def edit_version(file):
@@ -21,16 +22,16 @@ def edit_version(file):
         if '?v=' in line:
             a, c = line.split('?v=')
             b, c = v, c.split('"')[1]
-            line = a+b+c
+            line = a+'?v='+b+'"'+c
         edited += line+'\n'
 
-    with open(file, encoding='utf-8') as f: f.write(edited)
+    with open(file, 'w', encoding='utf-8') as f: f.write(edited)
     print('Edited', file)
 
 def edit_files(path):
     for f in listdir(path):
         f = join(path, f)
-        if isdir(f): find_files(f)
+        if isdir(f) and f not in ('.', '..'): edit_files(f)
         elif splitext(f)[1] == '.html':
             edit_version(f)
 
